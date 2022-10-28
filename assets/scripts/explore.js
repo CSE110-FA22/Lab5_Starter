1a9581fc67b6f3ed23345d6414bc95c3ae2a592a
Alberto Valencia <alvalenc@ucsd.edu>
2022-10-28 13:28:31 -0700

    Completed Speech synthesis. Need to change image

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,54 +4,59 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
-const synth = window.speechSynthesis;
-const inputForm = document.querySelector('form');
-const inputTxt = document.querySelector('text-to-speak');
-const voiceSelect = document.querySelector('select');
-const pitch = document.querySelector('#pitch');
-const pitchValue = document.querySelector('.pitch-value');
-const rate = document.querySelector('#rate');
-const rateValue = document.querySelector('.rate-value');
-
-let voices = [];
-
-function populateVoiceList() {
-  voices = synth.getVoices();
-
-  for (let i = 0; i < voices.length ; i++) {
-    const option = document.createElement('option');
-    option.textContent = `${voices[i].name} (${voices[i].lang})`;
-
-    if (voices[i].default) {
-      option.textContent += ' — DEFAULT';
+  const synth = window.speechSynthesis;
+  const faceImg = document.getElementsByTagName("img")[0]
+  const inputTxt = document.getElementById('text-to-speak');
+  const voiceSelect = document.getElementById('voice-select');
+  let voices = [];
+  function populateVoiceList() {
+    voices = synth.getVoices();
+    for (let i = 0; i < voices.length ; i++) {
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      if (voices[i].default) {
+        option.textContent += ' — DEFAULT';
+      }
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voiceSelect.appendChild(option);
     }
-
-    option.setAttribute('data-lang', voices[i].lang);
-    option.setAttribute('data-name', voices[i].name);
-    voiceSelect.appendChild(option);
   }
-}
 
-populateVoiceList();
+  populateVoiceList();
+  /*
+  voiceSelect.addEventListener("change", (event) =>{
+    if(synth.speaking === false){
+      faceImg.src = "assets/images/smiling.png"
+    }
+  });
+
+  inputTxt.addEventListener("input", (event) =>{
+    if(synth.speaking === false){
+      faceImg.src = "assets/images/smiling.png"
+    }
+  });
+*/
+  //synth.speaking.addEventListener( faceImg.src = "assets/images/smiling.png")
+  
+
   if (speechSynthesis.onvoiceschanged !== undefined) {
     speechSynthesis.onvoiceschanged = populateVoiceList;
   }
 
-  inputForm.onsubmit = (event) => {
+  document.getElementsByTagName("button")[0].addEventListener("click", (event) =>{
     event.preventDefault();
-
-    const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+    const utterThis = new SpeechSynthesisUtterance(inputTxt.value); 
     const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
     for (let i = 0; i < voices.length ; i++) {
       if (voices[i].name === selectedOption) {
         utterThis.voice = voices[i];
-      }
+      } 
     }
-    utterThis.pitch = pitch.value;
-    utterThis.rate = rate.value;
     synth.speak(utterThis);
-
+    faceImg.src = "assets/images/smiling-open.png";
     inputTxt.blur();
-  }
-  
+    
+  });
+ 
 }
