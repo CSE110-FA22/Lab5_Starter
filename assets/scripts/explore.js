6b024938a13e82e85c099d524f85896898410c8f
ioeddk <78573313+ioeddk@users.noreply.github.com>
2022-10-29 07:13:13 -0700

    fixed no-response bug
    identified that the no-response problem is caused by the browser's security protocol. Fixed by using a live server.

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,12 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+
 function init() {
-  // TODO
+  let button = document.getElementsByTagName('button')[0];
+  let smile = document.getElementsByTagName('img')[0];
+
+  button.addEventListener('click', function () {
+    smile.src = "assets/images/smiling-open.png";
+  });
 }
