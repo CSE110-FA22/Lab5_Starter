280f82477c447e6de9afbff9b575810f3ae79dd9
naseralemi12 <65462752+naseralemi12@users.noreply.github.com>
2022-10-28 23:41:33 -0700

    image and volume icone working properly

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,31 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+    // TODO
+    const selectElement = document.getElementById("horn-select");
+    const changeImage = document.getElementById("main img")
+    selectElement.addEventListener('change', (event) => {
+        if (event.target.value == "air-horn") {
+            document.querySelector("main img").src = "assets/images/air-horn.svg";
+        } else if (event.target.value == "car-horn") {
+            document.querySelector("main img").src = "assets/images/car-horn.svg";
+        } else if (event.target.value == "party-horn") {
+            document.querySelector("main img").src = "assets/images/party-horn.svg";
+        }
+    });
+
+
+    const volumeElement = document.getElementById("volume");
+    volumeElement.addEventListener('input', (event) => {
+        if (volumeElement.value == "0") {
+            document.querySelector("div img").src = "assets/icons/volume-level-0.svg";
+        } else if (volumeElement.value >= 1 && volumeElement.value < 33) {
+            document.querySelector("div img").src = "assets/icons/volume-level-1.svg";
+        } else if (volumeElement.value >= 33 && volumeElement.value < 67) {
+            document.querySelector("div img").src = "assets/icons/volume-level-2.svg";
+        } else if (volumeElement.value >= 67) {
+            document.querySelector("div img").src = "assets/icons/volume-level-3.svg";
+        }
+    });
+
 }
