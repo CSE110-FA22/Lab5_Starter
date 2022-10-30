8d89540854fed19839434a6e43bf308b04452b51
ioeddk <78573313+ioeddk@users.noreply.github.com>
2022-10-29 17:15:51 -0700

    populated voice list
    successfully populate the voice list. Found a demo in the documentation that demonstrates the same thing. What does the field speachSynthesis.onvoicechanged do? Without this line the voice list in the html is always blank. What we are doing when assigning it with populateVoiceLIst to it without paranthesis?

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,23 +1,41 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
-const speaker = window.speechSynthesis;
+let button = document.getElementsByTagName('button')[0];
+let smile = document.getElementsByTagName('img')[0];
+
+var speaker = window.speechSynthesis;
+var voice_select = document.getElementById('voice-select');
+var voices = [];
+
+//text_to_speak.value
+button.addEventListener('click', function () {
+
+  let text_to_speak = document.getElementById('text-to-speak');  // Use text_to_speak.value to access the text here. 
+  let utterance = new SpeechSynthesisUtterance(text_to_speak.value);  // The function create an utterance. 
+  speaker.speak(utterance); 
+  if (speaker.speaking) {
+    smile.src = "assets/images/smiling-open.png";
+  } 
+  smile.src = "assets/images/smiling.png";
+});
+
 
 function init() {
-  let button = document.getElementsByTagName('button')[0];
-  let smile = document.getElementsByTagName('img')[0];
-  
-  //text_to_speak.value
-  button.addEventListener('click', function () {
-
-    let text_to_speak = document.getElementById('text-to-speak');  // Use text_to_speak.value to access the text here. 
-    let utterance = new SpeechSynthesisUtterance(text_to_speak.value);  // The function create an utterance. 
-    speaker.speak(utterance); 
-    if (speaker.speaking) {
-      smile.src = "assets/images/smiling-open.png";
-    } 
-    smile.src = "assets/images/smiling.png";
-  });
-
   
 }
+
+function populateVoiceList () {
+  voices = speaker.getVoices();
+  for (let i = 0; i < voices.length; i++){
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name}`;
+    voice_select.appendChild(option);
+  }  
+}
+
+populateVoiceList();
+
+speechSynthesis.onvoiceschanged = populateVoiceList;
+
+
