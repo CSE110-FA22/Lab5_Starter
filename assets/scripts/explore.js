f80ecc6d9f9dd465ac7b751b16fc57e3e6b2af48
ioeddk <78573313+ioeddk@users.noreply.github.com>
2022-10-29 23:06:19 -0700

    basic speech

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,13 +1,23 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
-
+const speaker = window.speechSynthesis;
 
 function init() {
   let button = document.getElementsByTagName('button')[0];
   let smile = document.getElementsByTagName('img')[0];
-
+  
+  //text_to_speak.value
   button.addEventListener('click', function () {
-    smile.src = "assets/images/smiling-open.png";
+
+    let text_to_speak = document.getElementById('text-to-speak');  // Use text_to_speak.value to access the text here. 
+    let utterance = new SpeechSynthesisUtterance(text_to_speak.value);  // The function create an utterance. 
+    speaker.speak(utterance); 
+    if (speaker.speaking) {
+      smile.src = "assets/images/smiling-open.png";
+    } 
+    smile.src = "assets/images/smiling.png";
   });
+
+  
 }
