d27595e4e8113020c529f15323ebfb6961c2aa02
Brian Mendez <97647685+B1mendez@users.noreply.github.com>
2022-10-30 15:31:11 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -8,8 +8,9 @@ function init() {
   //Get element by Id for objects
   var volumeControl = document.getElementById('volume');
   var hornSelector = document.getElementById('horn-select');
+  var volumeIcon = documnet.querySelector("[alt='Volume level 2']");
   //Query selector for objects
-  var images = document.querySelectorAll('img');
+  var hornImages = document.querySelectorAll('img');
   var audioSounds = document.querySelector('audio');
   var executeSound = document.querySelector('button');
 
@@ -18,29 +19,35 @@ function init() {
 
   hornSelector.addEventListener('change', function() {
     if (hornSelector.value == "air-horn") {
-      images[0].src = 'assets/images/air-horn.svg';
+      hornImages[0].src = 'assets/images/air-horn.svg';
       audioSounds.src = 'assets/audio/air-horn.mp3';
     } else if (hornSelector.value == "car-horn") {
-      images[0].src = 'assets/images/car-horn.svg';
+      hornImages[0].src = 'assets/images/car-horn.svg';
       audioSounds.src = 'assets/audio/car-horn.mp3';
     } else if (hornSelector.value == "party-horn") {
-      images[0].src = 'assets/images/party-horn.svg';
+      hornImages[0].src = 'assets/images/party-horn.svg';
       audioSounds.src = 'assets/audio/party-horn.mp3';
     }
   });
 
-  volumeControl.addEventListener('change', function() {
-    audio.audioSounds = audioSounds.value / 100;
-    if (audioSounds.value == 0) {
-      images[1].src = 'assets/icons/volume-level-0.svg';
-    } else if (audioSounds.value < 33) {
-      images[1].src = 'assets/icons/volume-level-1.svg';
-    } else if (audioSounds.value < 67) {
-      images[1].src = 'assets/icons/volume-level-2.svg';
-    } else {
-      images[1].src = 'assets/icons/volume-level-3.svg';
+  volumeControl.addEventListener('input', function() {
+    if(volumeControl.value == 0){
+       volumeIcon.src = 'assets/icons/volume-level-0.svg';
+       audioSounds.volume = 0;
     }
-  });
+    else if(volumeControl.value < 33){
+      volumeIcon.src = 'assets/icons/volume-level-1.svg';
+      audioSounds.volume = volumeControl.value / 100;
+    }
+    else if(volumeControl.value < 67){
+      volumeIcon.src = 'assets/icons/volume-level-2.svg';
+      audioSounds.volume = volumeControl.value / 100;
+    }
+    else if(volumeControl.value < 100){
+      volumeIcon.src = 'assets/icons/volume-level-3.svg';
+      audioSounds.volume = volumeControl.value / 100;
+    }
+  })
 
   executeSound.addEventListener('click', function() {
     if (hornSelector.value == "party-horn"){
