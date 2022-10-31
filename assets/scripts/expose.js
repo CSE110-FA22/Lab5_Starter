2eb9549d8fb9f8c3586cc9c167e825393c26c2d5
Alyssa Kongswangwongsa <zazara60@gmail.com>
2022-10-30 18:00:48 -0700

    finally able to make image change with dropdown menu

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,23 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const selectElement = document.getElementById('horn-select');
+  const selectImage = document.querySelector("[src='assets/images/no-image.png']");
+  selectElement.addEventListener('change', (event) =>{
+    let choice = event.target.value;
+    if(choice == "air-horn"){
+      selectImage.src = "assets/images/air-horn.svg";
+    }
+    else if(choice == "car-horn"){
+      selectImage.src = "assets/images/car-horn.svg";
+    }
+    else if(choice == "party-horn"){
+      selectImage.src = "assets/images/party-horn.svg";
+    }
+    else{
+      selectImage.src = "assets/images/no-image.png";
+    }
+  });
 }
+
+
