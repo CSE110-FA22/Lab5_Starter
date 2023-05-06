e79b16ecd58ed0daafce9ad5ef7139301956da58
Jenny Quach <65835856+hoatuyet423@users.noreply.github.com>
2023-05-06 11:50:51 -0700

    added set Img and set Audio - still buggy

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,5 +4,32 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
-  document.getElementsByClassName("expose");
+  //let = document.getElementById("expose");
+
+  var horn = document.getElementById('horn-select');
+  var horn_choice = horn.value;
+
+  console.log("hello");
+  function setImg(){
+    console.log("hello");
+    var img = document.querySelector('img');
+
+    img.src = `assets/images/${horn_choice}.svg`;
+    console.log(horn_choice);
+
+  }
+
+  //change the image whenever the user chaneg the input of horn 
+  horn.addEventListener('change', setImg());
+
+  function setAudio(){
+    let audio = document.querySelector('audio');
+    audio.src = `assets/audio/${horn_choice}.mp3`;
+  }
+  horn.addEventListener('change', setAudio());
+
+
+
+
+
 }
