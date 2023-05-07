492af2cc41230f60fcd37e8a56bea862e55079c7
Maryamkusman <93306676+Maryamkusman@users.noreply.github.com>
2023-05-06 19:33:21 -0700

    changes

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -6,14 +6,14 @@ function init() {
   // TODO
   //let = document.getElementById("expose");
 
+
+
   var horn = document.getElementById('horn-select');
   var horn_choice = horn.value;
 
-  console.log("hello");
-  function setImg(){
-    console.log("hello");
+  //image
+  function setImg(images){
     var img = document.querySelector('img');
-
     img.src = `assets/images/${horn_choice}.svg`;
     console.log(horn_choice);
 
@@ -22,9 +22,10 @@ function init() {
   //change the image whenever the user chaneg the input of horn 
   horn.addEventListener('change', setImg());
 
-  function setAudio(){
-    let audio = document.querySelector('audio');
+  function setAudio(audio){
+    var audio = document.querySelector('audio');
     audio.src = `assets/audio/${horn_choice}.mp3`;
+
   }
   horn.addEventListener('change', setAudio());
 
