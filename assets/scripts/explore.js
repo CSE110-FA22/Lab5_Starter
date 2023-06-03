27e07bf560ccbee29863ea934aeaa7d4663d05fc
Sindhu Kothe <97674151+Sindhu-Kothe@users.noreply.github.com>
2023-06-03 14:25:25 -0700

    Update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,25 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  // Get reference to the voice select dropdown
+  const voiceSelect = document.getElementById('voiceSelect');
+
+  // Load available voices when the page is loaded
+  window.addEventListener('click', () => {
+    // Check if the SpeechSynthesis API is available in the browser
+    if ('speechSynthesis' in window) {
+      // Fetch the available voices
+      const voices = speechSynthesis.getVoices();
+
+      // Populate the voice select dropdown
+      voices.forEach((voice) => {
+        const option = document.createElement('option');
+        option.textContent = `${voice.name} (${voice.lang})`;
+        option.value = voice.voiceURI;
+        voiceSelect.appendChild(option);
+      });
+    }
+  });
+  
+  
 }
