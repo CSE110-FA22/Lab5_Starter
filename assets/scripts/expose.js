41a6e99e58bd19bcea72487e29a7dc17179b9c3c
Sindhu Kothe <97674151+Sindhu-Kothe@users.noreply.github.com>
2023-06-03 12:00:02 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,25 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
+  const select = document.querySelector("select");
+  const image = document.querySelector("img");
   
+  function ChangeHornImg(){
+    const selectedOption = select.options[select.selectedIndex];
+    
+    if(select.selectedIndex == 1){
+      var imagePath = assets/images/air-horn.svg;
+    } else if(select.selectedIndex == 2){
+      var imagePath = assets/images/car-horn.svg;
+    } else if(select.selectedIndex == 3){
+      var imagePath = assets/images/party-horn.svg;
+    } else {
+      var imagePath = assets/images/no-image.png;
+    }
+    
+    image.src = imagePath;
+  }
+  
+  select.addEventListener('change', ChangeHornImg());
+    
 }
