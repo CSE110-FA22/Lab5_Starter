b7151e73637a15b606b7662eebbb90a6e75dd971
Sindhu Kothe <97674151+Sindhu-Kothe@users.noreply.github.com>
2023-06-03 14:48:33 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -43,6 +43,7 @@ function init() {
     var iconPath = "";
     
     if (volumeValue >= 1 && volumeValue < 33){
+      console.log("small");
       iconPath = "assets/icons/volume-level-1.svg";
     } else if(volumeValue >=33 && volumeValue < 67){
       iconPath = "assets/icons/volume-level-2.svg";
