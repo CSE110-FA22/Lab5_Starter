1ca4244692c1ce0243adc1fdf32e92c18484627e
Sindhu Kothe <97674151+Sindhu-Kothe@users.noreply.github.com>
2023-06-04 10:33:39 -0700

    Update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -7,16 +7,14 @@ function init() {
   const voiceSelect = document.querySelector("select");
   const smileyFace = document.querySelector("img");
   
-  function getVoices(){
-    const voices = speechSynthesis.getVoices();
-    
-    voices.forEach((voice) => {
-        const option = document.createElement('option');
-        option.textContent = `${voice.name} (${voice.lang})`;
-        option.value = voice.voiceURI;
-        voiceSelect.appendChild(option);
-      });
-  }
+  const voices = speechSynthesis.getVoices();
+   
+  voices.forEach((voice) => {
+      const option = document.createElement('option');
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.value = voice.voiceURI;
+      voiceSelect.appendChild(option);
+    });
   
   smileyFace.addEventListner('click', getVoices);
   
